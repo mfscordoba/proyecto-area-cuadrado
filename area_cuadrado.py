1058f7bd32aae1daabb96b3939109f8c455d1d48
Manuel F <147614949+mfscordoba@users.noreply.github.com>
2024-06-30 11:23:22 +0200

    Create area_cuadrado.py

---
+++ area_cuadrado.py
@@ -0,0 +1,6 @@
+def area():
+    lado= float(input("Introduce el lado del cuadrado: "))
+    area =   lado * lado
+    print(f'El area del cuadrado con lado: {lado} por {lado} es {area} ')
+   
+area()
